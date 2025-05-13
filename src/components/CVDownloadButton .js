fc25316fe4a51d4e8d8aac5ecc2499f42e47277e
Luis Hostos <114273573+Luanho11@users.noreply.github.com>
2025-05-12 19:33:36 -0500

    Actualizado de botón de descarga
    No descargaba correctamente

--- src/components/CVDownloadButton .js
+++ src/components/CVDownloadButton .js
@@ -3,7 +3,10 @@ import './CVDownloadButton.css';
 
 const CVDownloadButton = () => {
     return (
-        <a href='${process.env.PUBLIC_URL}/CV_HostosLuis_Developer.pdf' download>
+        <a href={'${process.env.PUBLIC_URL}/CV_HostosLuis_Developer.pdf'}
+        download = "CV_HostosLuis_Developer.pdf"
+        target = "_blank"
+        rel = "noopener noreferrer">
             <button className="download-btn">Descargar CV</button>
         </a>
     )
